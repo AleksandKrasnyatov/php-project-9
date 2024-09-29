8252999f7b33290e14d4c8db0afd6ec0026ab15d
AlekasndKrasnyatov <ssokoloss1@gmail.com>
2024-09-29 10:33:59 +0300

    add repository and Url model

---
+++ src/Url.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+class Url
+{
+    private ?int $id = null;
+    private ?string $name = null;
+    private ?string $created_at = null;
+
+    public static function create(string $name, string $time = null): Url
+    {
+        $url = new Url();
+        $url->setName($name);
+        if ($time !== null) {
+            $url->setDateTime($time);
+        }
+        return $url;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getDateTime(): ?string
+    {
+        return $this->created_at;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function exists(): bool
+    {
+        return !is_null($this->getId());
+    }
+
+    public function setDateTime(string $time): void
+    {
+        $this->created_at = $time;
+    }
+
+    public function setCurrentTime(): void
+    {
+        $this->created_at = date("Y-m-d H:i:s");
+    }
+}
