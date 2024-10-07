61b760a24113d5d0974f3bd06dd190011d14197e
alex <ssokoloss1@gmail.com>
2024-10-07 09:44:43 +0300

    refactor validation

--- src/UrlValidator.php
+++ src/UrlValidator.php
@@ -12,19 +12,16 @@ class UrlValidator
         } else {
             if (strlen($urlData['name']) > 255) {
                 $errors['name'] = "max 255 symbols allowed";
-            } elseif (!filter_var($urlData['name'], FILTER_VALIDATE_URL)) {
-                $errors['name'] = "url is not valid";
+            } else {
+                $parsedUrl = parse_url($urlData['name']);
+                if (!array_key_exists('scheme', $parsedUrl) || !array_key_exists('host', $parsedUrl)) {
+                    $errors['name'] = "url is not valid";
+                } elseif (
+                    !in_array($parsedUrl['scheme'], ['http', 'https']) || !str_contains($parsedUrl['host'], '.')
+                ) {
+                    $errors['name'] = "Некорректный URL ";
+                }
             }
-            //            possible alternative
-//            } else {
-//                $parsedUrl = parse_url($urlData['name']);
-//                if (!array_key_exists('scheme', $parsedUrl) || !array_key_exists('host', $parsedUrl)) {
-//                    $errors['name'] = "url is not valid";
-//                } elseif (!in_array($parsedUrl['scheme'], ['http', 'https'])
-//                          || !str_contains($parsedUrl['host'], '.')) {
-//                    $errors['name'] = "url is not valid";
-//                }
-//            }
         }
         return $errors;
     }
